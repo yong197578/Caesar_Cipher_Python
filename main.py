659737860bc3b9c70c282f86da7221be1ef63dcf
Yong Choi <yong.s.choi2@gmail.com>
2023-08-25 11:53:35 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+alphabet = \
+    ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount = shift_amount * -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"Here's the {cipher_direction}d result: {end_text}")
+
+import art
+print(art.logo)
+
+should_end = False
+while not should_end:
+    direction = input("Type 'encode' to encrypt or 'decode' to decrypt \n").lower()
+    text = input("type your message here \n").lower()
+    shift = int(input("Type the shift number: \n"))
+
+    shift = shift % 26
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no' \n").lower()
+    if restart == "no":
+        should_end = True
+        print("Good Bye")
+
+
+
